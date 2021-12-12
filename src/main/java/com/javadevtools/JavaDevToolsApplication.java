e59608be9702ee3b92ab82677a6660ec9e820d19
Andrew Dingwall-Smith <andrew@dingwall-smith.com>
2021-12-12 17:10:46 +0000

    Initial commit

---
+++ src/main/java/com/javadevtools/JavaDevToolsApplication.java
@@ -0,0 +1,13 @@
+package com.javadevtools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDevToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDevToolsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javadevtools/JavaDevToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javadevtools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDevToolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
