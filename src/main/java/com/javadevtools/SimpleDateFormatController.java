b15ea4904c66effe821492e7a55b27732dc8e7cc
Andrew Dingwall-Smith <andrew@dingwall-smith.com>
2021-12-27 22:53:05 +0000

    Redirect to simpledateformat

--- src/main/java/com/javadevtools/SimpleDateFormatController.java
+++ src/main/java/com/javadevtools/SimpleDateFormatController.java
@@ -1,9 +1,10 @@
 package com.javadevtools;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.Map;
 
@@ -14,7 +15,7 @@ public class SimpleDateFormatController {
 
 	@Autowired
 	private JavaVersionService javaVersionService;
-	
+
 	@Autowired
 	private TimeZoneService timeZoneService;
 
@@ -24,5 +25,12 @@ public class SimpleDateFormatController {
 		model.put("timeZones", timeZoneService.getTimeZones());
 		model.put("javaVersion", javaVersionService.getJavaVersion());
 		return "simpledateformat";
+	}
+
+	@GetMapping(value = "/")
+	public RedirectView getPost() {
+		RedirectView redirectView = new RedirectView("/simpledateformat");
+		redirectView.setStatusCode(HttpStatus.MOVED_PERMANENTLY);
+		return redirectView;
 	}
 }
