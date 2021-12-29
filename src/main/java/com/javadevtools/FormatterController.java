be56fda23c07480592b65d3d47944068d0e0811d
Andrew Dingwall-Smith <andrew@dingwall-smith.com>
2021-12-29 20:26:04 +0000

    Refactor controllers

--- src/main/java/com/javadevtools/FormatterController.java
+++ src/main/java/com/javadevtools/FormatterController.java
@@ -7,12 +7,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class DateTimeFormatterController {
+public class FormatterController {
 	private final LocaleService localeService;
 	private final JavaVersionService javaVersionService;
 	private final TimeZoneService timeZoneService;
 	
-	public DateTimeFormatterController(
+	public FormatterController(
 			LocaleService localeService,
 			JavaVersionService javaVersionService,
 			TimeZoneService timeZoneService) {
@@ -20,9 +20,17 @@ public class DateTimeFormatterController {
 		this.javaVersionService = javaVersionService;
 		this.timeZoneService = timeZoneService;
 	}
+	
+	@GetMapping("/simpledateformat")
+	public String displaySimpleDateFormatPage(Map<String, Object> model) {
+		model.put("locales", localeService.getLocales());
+		model.put("timeZones", timeZoneService.getTimeZones());
+		model.put("javaVersion", javaVersionService.getJavaVersion());
+		return "simpledateformat";
+	}
 
 	@GetMapping("/datetimeformatter")
-	public String main(Map<String, Object> model) {
+	public String displayDateTimeFormatterPage(Map<String, Object> model) {
 		model.put("locales", localeService.getLocales());
 		model.put("timeZones", timeZoneService.getTimeZones());
 		model.put("javaVersion", javaVersionService.getJavaVersion());
