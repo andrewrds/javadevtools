006ad843d90f854d514892eeb408601226e7935b
Andrew Dingwall-Smith <andrew@dingwall-smith.com>
2021-12-30 13:55:28 +0000

    Handle format errors

--- src/main/java/com/javadevtools/dateformat/FormtterService.java
+++ src/main/java/com/javadevtools/dateformat/FormtterService.java
@@ -11,12 +11,12 @@ public class FormtterService {
 
 	public Map<String, Object> format(DateFormatParams params, IDateFormatterWrapper formatter) {
 		Map<String, Object> result = new HashMap<>();
-		
+
 		try {
 			Locale locale = Locale.forLanguageTag(params.getLocale());
 			String value = formatter.format(params.getPattern(), locale, params.getTimeZone());
 			result.put("value", value);
-		} catch (IllegalArgumentException e) {
+		} catch (RuntimeException e) {
 			result.put("value", e.getMessage());
 			result.put("error", true);
 		}
